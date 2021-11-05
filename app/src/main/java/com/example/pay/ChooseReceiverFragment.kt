7c600e03725e854f5a6fd3cabed55a41918a9df1
kalpesh022 <kalpeshdhupia@gmail.com>
2021-11-05 15:36:58 +0530

    checks

--- app/src/main/java/com/example/pay/ChooseReceiverFragment.kt
+++ app/src/main/java/com/example/pay/ChooseReceiverFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_choose_receiver.*
 
@@ -13,15 +14,20 @@ class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver) {
         super.onViewCreated(view, savedInstanceState)
         btn_next.setOnClickListener {
             val receiverName = et_receiver_name.text.toString()
+            if (receiverName.isNotEmpty()) {
+                val action = ChooseReceiverFragmentDirections
+                    .actionChooseReceiverFragmentToSendCashFragment(receiverName)
+                findNavController().navigate(action)
+            } else {
+                Toast.makeText(requireContext(), "Enter Name", Toast.LENGTH_SHORT).show()
+            }
 
-            val action = ChooseReceiverFragmentDirections
-                .actionChooseReceiverFragmentToSendCashFragment(receiverName)
-            findNavController().navigate(action)
 
         }
 
         btn_cancel.setOnClickListener {
-            val action = ChooseReceiverFragmentDirections.actionChooseReceiverFragmentToHomeFragment()
+            val action =
+                ChooseReceiverFragmentDirections.actionChooseReceiverFragmentToHomeFragment()
             findNavController().navigate(action)
         }
     }
--- app/src/main/java/com/example/pay/ViewBalanceFragment.kt
+++ app/src/main/java/com/example/pay/ViewBalanceFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pay
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -17,11 +18,21 @@ class ViewBalanceFragment : Fragment(R.layout.fragment_view_balance) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        SharedPref.init(requireContext())
+        val sharedPreferences =
+            this.activity?.getSharedPreferences("REM_BAL_INFO", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences?.edit()
+        editor?.putLong("Rem_Bal", remainingBal.toLong())
+        editor?.apply()
+
+        remainingBal = sharedPreferences?.getLong("Rem_Bal",0)?.toInt()!!
 
         val transactionAmount = arguments?.getLong("transAmt")
         remainingBal = (tvTotalBal.text.toString().toLong() - transactionAmount!!).toInt()
         tvTotalBal.text = remainingBal.toString()
+
+
+
 
 
     }
